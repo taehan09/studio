3c4e80e133ef7b277ad662d0bb7539c7d1f44806
김태한 <hanidas11@gmail.com>
2025-09-07 13:17:20 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "ashgrayink-shop",
-  "appId": "1:600881043133:web:688a3f08fb3bbe238609d4",
-  "storageBucket": "ashgrayink-shop.firebasestorage.app",
-  "apiKey": "AIzaSyDW5y_qS5_Abqbp7EkROk7lbJyQvMrzauk",
-  "authDomain": "ashgrayink-shop.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "600881043133"
+  apiKey: "AIzaSyDW5y_qS5_Abqbp7EkROk7lbJyQvMrzauk",
+  authDomain: "ashgrayink-shop.firebaseapp.com",
+  projectId: "ashgrayink-shop",
+  storageBucket: "ashgrayink-shop.firebasestorage.app",
+  messagingSenderId: "600881043133",
+  appId: "1:600881043133:web:78a3f8a670f45b988609d4"
 };
+
 
 // Initialize Firebase for the client
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
